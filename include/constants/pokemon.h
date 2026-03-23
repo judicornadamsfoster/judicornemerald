5cbfbce0d8cdd308110c02ca3bc2b93c91fdde4a
judicornadamsfoster <judicornadamsfoster@gmail.com>
2026-03-23 15:37:35 +0000

    new evolution method EVO_ITEM_HOLD

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -259,6 +259,7 @@
 #define EVO_LEVEL_NINJASK    13 // Pokémon reaches the specified level (special value for Ninjask)
 #define EVO_LEVEL_SHEDINJA   14 // Pokémon reaches the specified level (special value for Shedinja)
 #define EVO_BEAUTY           15 // Pokémon levels up with beauty ≥ specified value
+#define EVO_ITEM_HOLD        16 // Pokémon levels up, holds specified item
 
 #define EVOS_PER_MON 8
 
