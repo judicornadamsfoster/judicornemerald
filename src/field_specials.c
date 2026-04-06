2a89f138934262361f538e62e9f536be54ce9429
judicornadamsfoster <judicornadamsfoster@gmail.com>
2026-04-06 19:48:17 +0100

    special CheckSpecies added
    new special CheckSpecies and code for if have a glaceon and leafeon in party

--- src/field_specials.c
+++ src/field_specials.c
@@ -4348,3 +4348,27 @@ void GiveRandomEgg(void)
     // Return value ignored (should only ever go to party)
     GiveMonToPlayer(&mon);
 }
+
+void CheckSpecies(void)
+{
+    u8 i;
+    u16 species;
+    struct Pokemon *pokemon;
+    for (i = 0; i < CalculatePlayerPartyCount(); i++)
+    {
+        pokemon = &gPlayerParty[i];
+        if (GetMonData(pokemon, MON_DATA_SANITY_HAS_SPECIES) && !GetMonData(pokemon, MON_DATA_IS_EGG))
+        {
+            species = GetMonData(pokemon, MON_DATA_SPECIES);
+            if (species == gSpecialVar_0x8005)
+            {
+                gSpecialVar_Result = TRUE;
+                return;
+            }
+            else
+            {
+                gSpecialVar_Result = FALSE;
+            }
+        }
+    }
+}
